8707fc79164fe50e68146f674f6b037acfaa2bac
esaaprillia <esaapriliasalsabila@gmail.com>
2026-07-03 19:13:12 +0800

    Add files via upload

---
+++ web/js/version.js
@@ -0,0 +1,8 @@
+/**
+ * LightNVR version information
+ * This file is auto-generated from CMakeLists.txt during the build process
+ * DO NOT EDIT MANUALLY
+ */
+
+export const VERSION = '0.35.4';
+export const GIT_COMMIT = 'e33ce83c';
